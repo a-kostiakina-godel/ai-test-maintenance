a60b3c4a9bfc0d0289acd788de8890c40126bfba
Anastasiia Kostiakina <a.kostiakina@godeltech.com>
2026-05-21 00:09:30 +0200

    Add update for ci env

--- tests/pages/cliPage.ts
+++ tests/pages/cliPage.ts
@@ -2,7 +2,7 @@ import { Page } from '@playwright/test';
 import { BasePage } from './basePage';
 
 export class CliPage extends BasePage {
-  readonly mainHeading = this.page.getByRole('heading', { name: 'Playwright CLI' });
+  readonly mainHeading = this.page.getByRole('heading', { name: 'Playwright CLI', exact: true });
 
   constructor(page: Page) {
     super(page);
